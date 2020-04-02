41b8f49371d6d88a4c6d1f52cebdc0e7ef3d59f7
Michael Silva <michaelfs@ciandt.com>
2020-04-01 21:37:54 -0300

    Added lombok to models

--- src/main/java/com/marvel/api/model/Character.java
+++ src/main/java/com/marvel/api/model/Character.java
@@ -5,9 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Data @NoArgsConstructor @AllArgsConstructor //lombok frees you from generating all the code commented
 public class Character {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -20,7 +24,7 @@ public class Character {
 //	private List<String> movies;
 //	private List<String> series;
 	
-	public Character() {}
+//	public Character() {}
 
 	public Character(String name, String description, String superPowers) {
 		super();
@@ -29,45 +33,45 @@ public class Character {
 		this.superPowers = superPowers;
 	}
 
-	public Character(Long id, String name, String description, String superPowers) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.superPowers = superPowers;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public String getSuperPowers() {
-		return superPowers;
-	}
-	
-	public void setSuperPowers(String superPowers) {
-		this.superPowers = superPowers;
-	}
+//	public Character(Long id, String name, String description, String superPowers) {
+//		super();
+//		this.id = id;
+//		this.name = name;
+//		this.description = description;
+//		this.superPowers = superPowers;
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public String getSuperPowers() {
+//		return superPowers;
+//	}
+//
+//	public void setSuperPowers(String superPowers) {
+//		this.superPowers = superPowers;
+//	}
 	
 //	public List<String> getMovies() {
 //		return movies;
@@ -85,30 +89,30 @@ public class Character {
 //		this.series = series;
 //	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Character other = (Character) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((name == null) ? 0 : name.hashCode());
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Character other = (Character) obj;
+//		if (name == null) {
+//			if (other.name != null)
+//				return false;
+//		} else if (!name.equals(other.name))
+//			return false;
+//		return true;
+//	}
 	
 //	@Override
 //	public String toString() {
